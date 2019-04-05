2f2af63a951f055bfc51be2c8a9568a21bd59d38
Minh Hoang NGUYEN <mhoang8891>
2019-04-05 17:55:58 +0700

    stdin func added & add 2 lines to text

--- app_test/function_test.py
+++ app_test/function_test.py
@@ -1,7 +1,14 @@
+import sys
+
+
 def double123():
     for i in [1, 2, 3]:
         print((lambda x: x * i)(2))
     print("end")
 
 
-double123()
+count = 0
+for line in sys.stdin:
+    count += 1
+
+print(count)
